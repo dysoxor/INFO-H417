f6fe889362a3225771ec3dc706bf7a28319d03fb
neji601 <alexandremissenard@gmail.com>
2020-12-16 12:21:22 +0100

    Add files via upload

---
+++ Experiment1.h
@@ -0,0 +1,20 @@
+#ifndef PROJECT_EXPERIMENT1_H
+#define PROJECT_EXPERIMENT1_H
+
+#include "InputStream1.h"
+#include "InputStream2.h"
+#include "InputStream3.h"
+#include "InputStream4.h"
+#include <iostream>
+using namespace std;
+
+class Experiment1{
+
+public:
+  void length1(string f);
+  void length2(string f);
+  void length3(string f);
+  void length4(string f);
+};
+
+#endif //PROJECT_EXPERIMENT1_H
